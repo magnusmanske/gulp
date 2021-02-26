e8edd6505c244eff5865682867be20dd40972f05
Magnus Manske <mm6@sanger.ac.uk>
2021-02-26 15:42:27 +0000

    misc

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 //#[macro_use]
 extern crate serde_json;
+#[macro_use]
+extern crate rocket;
 
 pub mod list ;
 pub mod app_state;
@@ -7,7 +9,7 @@ pub mod gulp_response;
 
 use tokio::fs::File as TokioFile;
 use tokio_util::codec::{BytesCodec, FramedRead};
-//use qstring::QString;
+use qstring::QString;
 //use crate::form_parameters::FormParameters;
 use app_state::AppState;
 use gulp_response::{ContentType,GulpResponse};
@@ -17,12 +19,69 @@ use std::env;
 use std::fs::File;
 use std::sync::Arc;
 use std::{net::SocketAddr};
-use hyper::{header, Body, Request, Response, Server, Error, StatusCode, Method};
-use hyper::service::{make_service_fn, service_fn};
+//use hyper::{header, Body, Request, Response, Server, Error, StatusCode, Method};
+//use hyper::service::{make_service_fn, service_fn};
 
+
+#[get("/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[launch]
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/hello", routes![hello])
+}
+
+#[tokio::main]
+async fn main() -> Result<(),Error> {
+    let basedir = env::current_dir()
+        .expect("Can't get CWD")
+        .to_str()
+        .expect("Can't convert CWD to_str")
+        .to_string();
+    let path = basedir.to_owned() + "/config.json";
+    let file = File::open(&path).unwrap_or_else(|_| panic!("Can not open config file at {}", &path));
+    let petscan_config: Value =
+        serde_json::from_reader(file).expect("Can not parse JSON from config file");
+    rocket();
+
+    /*
+    let ip_address = petscan_config["http_server"].as_str().unwrap_or("0.0.0.0").to_string();
+    let port = petscan_config["http_port"].as_u64().unwrap_or(80) as u16;
+    let app_state = Arc::new(AppState::new_from_config(&petscan_config).await) ;
+
+    let ip_address : Vec<u8> = ip_address.split('.').map(|s|s.parse::<u8>().unwrap()).collect();
+    let ip_address = std::net::Ipv4Addr::new(ip_address[0],ip_address[1],ip_address[2],ip_address[3],);
+    let addr = SocketAddr::from((ip_address, port));
+
+    let make_service = make_service_fn(move |_| {
+        let app_state = app_state.clone();
+
+        async {
+            Ok::<_, Error>(service_fn(move |req|  {
+                process_request(req,app_state.to_owned())
+            }))
+        }
+    });
+
+    let server = Server::bind(&addr).serve(make_service);
+
+    println!("Listening on http://{}", addr);
+
+    server.await?;
+*/
+    Ok(())
+}
+
+
+/*
 static NOTFOUND: &[u8] = b"Not Found";
 
 async fn process_form(parameters:&str, state: Arc<AppState>) -> GulpResponse {
+    let parameter_pairs = QString::from(parameters) ;
+    let parameter_pairs = parameter_pairs.to_pairs() ;
+    println!("{:?}",parameter_pairs);
     GulpResponse {
         s:"this is a GulpResponse".to_string(),
         content_type:ContentType::Plain
@@ -44,9 +103,9 @@ async fn simple_file_send(filename: &str,content_type: &str) -> Result<Response<
         let stream = FramedRead::new(file, BytesCodec::new());
         let body = Body::wrap_stream(stream);
         let response = Response::builder()
-        .header(header::CONTENT_TYPE, content_type)
-        .body(body)
-        .unwrap();
+            .header(header::CONTENT_TYPE, content_type)
+            .body(body)
+            .unwrap();
         return Ok(response);
     }
 
@@ -133,3 +192,5 @@ async fn main() -> Result<(),Error> {
 
     Ok(())
 }
+*/
+
