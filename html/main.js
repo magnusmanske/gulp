6acf0ca081c5c4a7269ca57ba7b730f0aca17ed8
Magnus Manske <mm6@sanger.ac.uk>
2023-02-08 15:42:45 +0000

    misc

--- html/main.js
+++ html/main.js
@@ -1,8 +1,13 @@
 var user ;
+var user_lists = [];
 
 function load_user_lists() {
     $.get("/auth/lists/write,admin",function(d){
-        console.log(d);
+        if ( d.status=="OK" ) user_lists = d.lists;
+        else {
+            user_lists = [];
+            console.log("Could not load lists for user");
+        }
     },"json")
 }
 
@@ -10,7 +15,6 @@ $(document).ready(function(){
     console.log("Ready");
     $.get("/auth/info",function(d){
         user = d.user;
-        console.log(user);
         if ( user===null ) {
             $("#user_greeting").text("");
             $("#login_logout").text("Log in").attr("href","/auth/login");
