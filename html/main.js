aa03376b4c1f4a0b16b815422da1a63b5c483a79
Magnus Manske <magnusmanske@googlemail.com>
2023-02-08 16:09:49 +0000

    vue first light

--- html/main.js
+++ html/main.js
@@ -1,3 +1,6 @@
+var router ;
+var app ;
+let wd = new WikiData() ;
 var user ;
 var user_lists = [];
 
@@ -17,11 +20,42 @@ $(document).ready(function(){
         user = d.user;
         if ( user===null ) {
             $("#user_greeting").text("");
-            $("#login_logout").text("Log in").attr("href","/auth/login");
+            $("#login_logout").html("<span tt='logout'></span>").attr("href","/auth/login");
         } else {
             $("#user_greeting").text(user.username);
-            $("#login_logout").text("Log out").attr("href","/auth/logout");
+            $("#login_logout").html("<span tt='logout'></span>").attr("href","/auth/logout");
             load_user_lists();
         }
+        tt.updateInterface($('#login_logout')) ;
     },"json");
 })
+
+
+$(document).ready ( function () {
+    vue_components.toolname = 'gulp' ;
+    Promise.all ( [
+            vue_components.loadComponents ( ['tool-translate','wd-link',//'autodesc','wikidatamap','wd-date','tool-navbar','commons-thumbnail',
+                'vue-components/main_page.html',
+                ] ) ,
+//            new Promise(function(resolve, reject) { loadPropertyCache ( resolve ) } )
+    ] ) .then ( () => {
+        current_language = tt.language ;
+        wd_link_wd = wd ;
+        tt.addILdropdown ( '#tooltranslate_wrapper' ) ;
+
+        const routes = [
+          { path: '/', component: MainPage },
+/*
+          { path: '/group', component: CatalogGroup , props:true },
+          { path: '/group/:key', component: CatalogGroup , props:true },
+*/
+        ] ;
+
+        router = new VueRouter({routes}) ;
+        app = new Vue ( { router } ) .$mount('#app') ;
+        setTimeout(function(){
+            tt.updateInterface($('body')) ;
+        },100);
+    } ) ;
+
+} ) ;
