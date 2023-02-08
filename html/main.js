8ed13c97bcad2b105ba9c1618e65ca6fa7293641
Magnus Manske <magnusmanske@googlemail.com>
2023-02-08 20:32:21 +0000

    user list loading

--- html/main.js
+++ html/main.js
@@ -2,20 +2,10 @@ var router ;
 var app ;
 let wd = new WikiData() ;
 var user ;
-var user_lists = [];
 
-function load_user_lists() {
-    $.get("/auth/lists/write,admin",function(d){
-        if ( d.status=="OK" ) user_lists = d.lists;
-        else {
-            user_lists = [];
-            console.log("Could not load lists for user");
-        }
-    },"json")
-}
 
-$(document).ready(function(){
-    console.log("Ready");
+
+function load_user_data(callback) {
     $.get("/auth/info",function(d){
         user = d.user;
         if ( user===null ) {
@@ -24,20 +14,19 @@ $(document).ready(function(){
         } else {
             $("#user_greeting").text(user.username);
             $("#login_logout").html("<span tt='logout'></span>").attr("href","/auth/logout");
-            load_user_lists();
         }
-        tt.updateInterface($('#login_logout')) ;
+        //tt.updateInterface($('#login_logout')) ;
+        callback();
     },"json");
-})
+}
 
-
-$(document).ready ( function () {
+$(document).ready(function(){
     vue_components.toolname = 'gulp' ;
     Promise.all ( [
             vue_components.loadComponents ( ['tool-translate','wd-link',//'autodesc','wikidatamap','wd-date','tool-navbar','commons-thumbnail',
                 'vue_components/main_page.html',
                 ] ) ,
-//            new Promise(function(resolve, reject) { loadPropertyCache ( resolve ) } )
+            new Promise(function(resolve, reject) { load_user_data ( resolve ) } )
     ] ) .then ( () => {
         current_language = tt.language ;
         wd_link_wd = wd ;
