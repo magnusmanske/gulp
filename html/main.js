41a223cd5a90abf04dbb7b4c08d6af568bb3d650
Magnus Manske <magnusmanske@googlemail.com>
2023-02-09 09:09:04 +0000

    list

--- html/main.js
+++ html/main.js
@@ -5,18 +5,15 @@ var user ;
 
 
 
-function load_user_data(callback) {
-    $.get("/auth/info",function(d){
-        user = d.user;
-        if ( user==null ) {
-            $("#user_greeting").text("");
-            $("#login_logout").html("<span tt='login'></span>").attr("href","/auth/login");
-        } else {
-            $("#user_greeting").text(user.username);
-            $("#login_logout").html("<span tt='logout'></span>").attr("href","/auth/logout");
-        }
-        callback();
-    },"json");
+function set_user_data(d) {
+    user = d.user;
+    if ( user==null ) {
+        $("#user_greeting").text("");
+        $("#login_logout").html("<span tt='login'></span>").attr("href","/auth/login");
+    } else {
+        $("#user_greeting").text(user.username);
+        $("#login_logout").html("<span tt='logout'></span>").attr("href","/auth/logout");
+    }
 }
 
 $(document).ready(function(){
@@ -24,8 +21,15 @@ $(document).ready(function(){
     Promise.all ( [
             vue_components.loadComponents ( ['tool-translate','wd-link',//'autodesc','wikidatamap','wd-date','tool-navbar','commons-thumbnail',
                 'vue_components/main_page.html',
+                'vue_components/list_page.html',
+                'vue_components/list.html',
                 ] ) ,
-            new Promise(function(resolve, reject) { load_user_data ( resolve ) } )
+            new Promise(function(resolve, reject) {
+                fetch("/auth/info")
+                    .then((response) => response.json())
+                    .then((d)=>set_user_data(d))
+                    .then(resolve)
+            } )
     ] ) .then ( () => {
         current_language = tt.language ;
         wd_link_wd = wd ;
@@ -33,6 +37,7 @@ $(document).ready(function(){
 
         const routes = [
           { path: '/', component: MainPage },
+          { path: '/list/:list_id', component: ListPage , props:true },
 /*
           { path: '/group', component: CatalogGroup , props:true },
           { path: '/group/:key', component: CatalogGroup , props:true },
